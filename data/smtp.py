22862ba7a3ebc3f84ce723d3c694b9ef4cc2f1c6
zanyarjamal <sleepyhacks@gmail.com>
2018-09-13 07:43:39 +0000

    Update smtp.py

--- data/smtp.py
+++ data/smtp.py
@@ -1,4 +1,4 @@
-smtps = "smtp.sendgrid.net"
-port = "587"
-username = "apikey"
-password = "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"
+smtps = "0"
+port = "0"
+username = "0"
+password = "0"
